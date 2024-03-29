d6dcee3951618dca12a2739eb59c4f48aea5a279
Lee, Jeong Han <jeonghan.lee@gmail.com>
2024-03-28 21:34:19 -0700

    add pvaccess support, check_release = no

--- cmd/st.cmd
+++ cmd/st.cmd
@@ -15,7 +15,7 @@ epicsEnvSet("CF_USERNAME", "jhlee")
 epicsEnvSet("LOG_PORT", "7002")
 epicsEnvSet("LOG_DEST", "10.0.0.2")
 
-#-# Register all support components
+#-- Register all support components
 dbLoadDatabase "dbd/ServiceTestIOC.dbd"
 ServiceTestIOC_registerRecordDeviceDriver pdbbase
 
@@ -26,20 +26,25 @@ dbLoadRecords("$(DB_TOP)/dbExample2.db" "user=$(IOCNAME), no=3, scan=5 second")
 
 dbLoadRecords("$(DB_TOP)/dbSubExample.db", "user=$(IOCNAME)")
 
-#-# EPICS BASE
-#-iocshLoad("$(IOCSH_TOP)/iocLog.iocsh",    "IOCNAME=$(IOCNAME), LOG_INET=$(LOG_DEST), LOG_INET_PORT=$(LOG_PORT)")
+#- EPICS BASE
+#iocshLoad("$(IOCSH_TOP)/iocLog.iocsh",    "IOCNAME=$(IOCNAME), LOG_INET=$(LOG_DEST), LOG_INET_PORT=$(LOG_PORT)")
 
-#-# Load record instances
+#-- Load record instances
 iocshLoad("$(IOCSH_TOP)/iocStats.iocsh",  "IOCNAME=$(IOCNAME), DATABASE_TOP=$(DB_TOP)")
 iocshLoad("$(IOCSH_TOP)/reccaster.iocsh", "IOCNAME=$(IOCNAME), DATABASE_TOP=$(DB_TOP)")
 #-iocshLoad("$(IOCSH_TOP)/caPutLog.iocsh",  "IOCNAME=$(IOCNAME), LOG_INET=$(LOG_DEST), LOG_INET_PORT=$(LOG_PORT)")
 #iocshLoad("$(IOCSH_TOP)/autosave.iocsh", "AS_TOP=$(TOP),IOCNAME=$(IOC)-as,DATABASE_TOP=$(DB_TOP),SEQ_PERIOD=60")
 #iocshLoad("$(IOCSH_TOP)/motorSim.iocsh",  "P=$(SYSSUBSYS), D=$(DEVDI), DATABASE_TOP=$(DB_TOP)")
 
-
+#>>>>>>>>>>>>>
 iocInit()
-
-dbl > "$(SYSSUBSYS)_$(DEVDI)_PVs.list"
+#>>>>>>>>>>>>>
+#
+pvasr
+ClockTime_Report
+#--
+epicsEnvShow > ${IOCNAME}.softioc¶
+dbl > ${IOCNAME}.list
 
 seq sncExample, "user=$(IOCNAME)"
 
