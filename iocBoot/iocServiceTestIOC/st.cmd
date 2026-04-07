6157d74346202b456c2b9dafa067e8cd44322f47
Lee, Jeong Han <jeonghan.lee@gmail.com>
2026-04-06 21:44:31 -0700

    update st.cmd location to the standard path

--- iocBoot/iocServiceTestIOC/st.cmd
+++ iocBoot/iocServiceTestIOC/st.cmd
@@ -1,21 +1,56 @@
 #!../../bin/linux-x86_64/ServiceTestIOC
 
-#- You may have to change ServiceTestIOC to something else
-#- everywhere it appears in this file
-
 < envPaths
 
-cd "${TOP}"
+epicsEnvSet("DB_TOP", "$(TOP)/db")
+epicsEnvSet("IOCSH_LOCAL_TOP",   "$(TOP)/iocsh")
 
-## Register all support components
-dbLoadDatabase "dbd/ServiceTestIOC.dbd"
+epicsEnvSet("LOCATION","TESTLAB")
+
+
+epicsEnvSet("SYSSUBSYS","LBNL")
+epicsEnvSet("DEVDI", "TESTIOC")
+epicsEnvSet("IOCNAME", "$(SYSSUBSYS):$(DEVDI)")
+epicsEnvSet("CF_USERNAME", "jhlee")
+
+epicsEnvSet("LOG_PORT", "7002")
+epicsEnvSet("LOG_DEST", "10.0.0.2")
+
+#-- Register all support components
+dbLoadDatabase "$(TOP)/dbd/ServiceTestIOC.dbd"
 ServiceTestIOC_registerRecordDeviceDriver pdbbase
 
-## Load record instances
-#dbLoadRecords("db/xxx.db","user=jhlee")
 
 cd "${TOP}/iocBoot/${IOC}"
-iocInit
 
-## Start any sequence programs
-#seq sncxxx,"user=jhlee"
+
+dbLoadRecords("$(DB_TOP)/dbExample1.db" "user=$(IOCNAME)")
+dbLoadRecords("$(DB_TOP)/dbExample2.db" "user=$(IOCNAME), no=1, scan=1 second")
+dbLoadRecords("$(DB_TOP)/dbExample2.db" "user=$(IOCNAME), no=2, scan=2 second")
+dbLoadRecords("$(DB_TOP)/dbExample2.db" "user=$(IOCNAME), no=3, scan=5 second")
+
+dbLoadRecords("$(DB_TOP)/dbSubExample.db", "user=$(IOCNAME)")
+
+#- EPICS BASE
+#iocshLoad("$(IOCSH_TOP)/iocLog.iocsh",    "IOCNAME=$(IOCNAME), LOG_INET=$(LOG_DEST), LOG_INET_PORT=$(LOG_PORT)")
+
+#-- Load record instances
+#iocshLoad("$(IOCSH_TOP)/iocStats.iocsh",  "IOCNAME=$(IOCNAME), DATABASE_TOP=$(DB_TOP)")
+#iocshLoad("$(IOCSH_TOP)/reccaster.iocsh", "IOCNAME=$(IOCNAME), DATABASE_TOP=$(DB_TOP)")
+#-iocshLoad("$(IOCSH_TOP)/caPutLog.iocsh",  "IOCNAME=$(IOCNAME), LOG_INET=$(LOG_DEST), LOG_INET_PORT=$(LOG_PORT)")
+#iocshLoad("$(IOCSH_TOP)/autosave.iocsh", "AS_TOP=$(TOP),IOCNAME=$(IOC)-as,DATABASE_TOP=$(DB_TOP),SEQ_PERIOD=60")
+#iocshLoad("$(IOCSH_TOP)/motorSim.iocsh",  "P=$(SYSSUBSYS), D=$(DEVDI), DATABASE_TOP=$(DB_TOP)")
+
+#>>>>>>>>>>>>>
+iocInit()
+#>>>>>>>>>>>>>
+#
+#pvasr
+ClockTime_Report
+#--
+epicsEnvShow > ${IOCNAME}.softioc
+dbl > ${IOCNAME}.list
+
+seq sncExample, "user=$(IOCNAME)"
+
+#EOF
